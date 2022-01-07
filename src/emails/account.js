ffa979b7e606a6d154ee50a8688ae59280b3b8f4
Nick Blum <nick.blum@protonmail.com>
2022-01-07 15:30:15 -0500

    setup environmental variables (db,email,jwt)

--- src/emails/account.js
+++ src/emails/account.js
@@ -1,12 +1,10 @@
-const sgMail = require('@sendgrid/mail')
-const sendgridAPIKey = ''
-
-//sgMail.setApiKey(sendgridAPIKey)
+// const sgMail = require('@sendgrid/mail')
+// sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
 const sendWelcomeEmail = (email, name) => {
     // sgMail.send({
     //     to: email,
-    //     from: 'nick.blum@protonmail.com',
+    //     from: process.env.SENDGRID_EMAIL,
     //     subject: `Welcome to our testing app!`,
     //     text: `I sent a test email, ${name}. I hope that I didn't get caught in a spam filter!`
     // })
@@ -17,7 +15,7 @@ const sendWelcomeEmail = (email, name) => {
 const sendCancelationEmail = (email, name) => {
     // sgMail.send({
     //     to: email,
-    //     from: 'nick.blum@protonmail.com',
+    //     from: process.env.SENDGRID_EMAIL,
     //     subject: `You've been unsubscribed`,
     //     text: `I sent a test email, ${name}. I hope that I didn't get caught in a spam filter!`
     // })
--- src/models/user.js
+++ src/models/user.js
@@ -81,7 +81,7 @@ userSchema.methods.toJSON = function(){
 // Generate user specific web token
 userSchema.methods.generateAuthToken = async function() {
     const user = this
-    const token = jwt.sign({_id: user._id.toString()},'myuniquecharacters')
+    const token = jwt.sign({_id: user._id.toString()},process.env.JWT_SECRET)
 
     user.tokens = user.tokens.concat({token})
     await user.save()
